fb109abbf719aadedaf42565da4f2eb70ddb1594
sun <1965190613@qq.com>
2019-03-07 18:06:08 +0800

    完成习题1.13

---
+++ lab1/print_asm.asm
@@ -0,0 +1,14 @@
+section data
+msg db "Hello World",0xA
+len equ $-msg
+section .text
+	global _start
+_start:
+	mov eax,4
+	mov ebx,1
+	mov ecx,msg
+	mov edx,len
+	int 0x80
+	mov eax,1
+	xor ebx,ebx
+	int 0x80
