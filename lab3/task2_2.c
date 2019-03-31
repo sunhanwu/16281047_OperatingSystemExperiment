34c76f1158433d3ead014dd921ba52fa6340f086
tencent <1965190613@qq.com>
2019-04-01 00:49:02 +0800

    fix some bug in task2_1 and add task2_2 code file and exeu file

--- lab3/task2_2.c
+++ lab3/task2_2.c
@@ -6,7 +6,7 @@
 #include<fcntl.h>
 #include<string.h>
 volatile int ticketCount=1000;
-/* sem_t *flag=NULL; */
+sem_t *flag=NULL;
 void *SaleThread(void *arg)
 {
 	int num,temp;
@@ -15,7 +15,7 @@ void *SaleThread(void *arg)
 	{
 		if(i % 10 ==0)
 			printf("卖%d张票,剩余%d张票\n",i,ticketCount);
-		/* sem_wait(flag); */
+		sem_wait(flag);
 		temp=ticketCount;
 		//放弃CPU，强制切换到另外一个进程
 		pthread_yield();
@@ -23,7 +23,7 @@ void *SaleThread(void *arg)
 		ticketCount-=1;
 		pthread_yield();
 		ticketCount=temp;
-		/* sem_post(flag); */
+		sem_post(flag);
 	}
 	return NULL;
 }
@@ -36,15 +36,14 @@ void *RefundThread(void *arg)
 	{
 		if(i % 10 ==0)
 			printf("退%d张票，剩余%d张票\n",i,ticketCount);
-		/* sem_wait(flag); */
+		sem_wait(flag);
 		temp=ticketCount;
 		pthread_yield();
 		temp=temp+1;
 		ticketCount+=1;
 		pthread_yield();
 		ticketCount=temp;
-		pthread_yield();
-		/* sem_post(flag); */
+		sem_post(flag);
 	}
 	return NULL;
 }
@@ -55,17 +54,17 @@ int main(int argc,char *argv[])
 		printf("请正确输入参数！\n");
 		exit(0);
 	}
-	/* flag=sem_open("flag",O_CREAT,0666,1); */
+	flag=sem_open("flag",O_CREAT,0666,1);
 	printf("初始票数为：%d\n",ticketCount);
 	pthread_t p1,p2;
-	/* printf("%s %s",argv[1],argv[2]); */
+	printf("%s %s",argv[1],argv[2]);
 	pthread_create(&p1,NULL,SaleThread,argv[1]);
 	pthread_create(&p2,NULL,RefundThread,argv[2]);
 	pthread_join(p1,NULL);
 	pthread_join(p2,NULL);
 	printf("最终票数为：%d\n",ticketCount);
-	/* sem_close(flag); */
-	/* sem_unlink("flag"); */
+	sem_close(flag);
+	sem_unlink("flag");
 	return 0;
 
 }
