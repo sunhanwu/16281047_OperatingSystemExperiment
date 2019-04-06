d0a096bf8002c95776d7bb36fa24474f1cf03c78
sun-macbook <1965190613@qq.com>
2019-04-06 20:53:03 +0800

    添加lab3-task4_a实验代码，receiver.c和sender.c

---
+++ lab3/Sender.c
@@ -0,0 +1,89 @@
+/*
+ * Filename: Sender.c
+ * Description: 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    key_t  key;
+    int shm_id;
+    int sem_id;
+    int value = 0;
+
+    //1.Product the key
+    key = ftok(".", 0xFF);
+
+    //2. Creat semaphore for visit the shared memory
+    sem_id = semget(key, 1, IPC_CREAT|0644);
+    if(-1 == sem_id)
+    {
+        perror("semget");
+        exit(EXIT_FAILURE);
+    }
+
+    //3. init the semaphore, sem=0
+    if(-1 == (semctl(sem_id, 0, SETVAL, value)))
+    {
+        perror("semctl");
+        exit(EXIT_FAILURE);
+    }
+
+    //4. Creat the shared memory(1K bytes)
+    shm_id = shmget(key, 1024, IPC_CREAT|0644);
+    if(-1 == shm_id)
+    {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
+
+    //5. attach the shm_id to this process
+    char *shm_ptr;
+    shm_ptr = shmat(shm_id, NULL, 0);
+    if(NULL == shm_ptr)
+    {
+        perror("shmat");
+        exit(EXIT_FAILURE);
+    }
+
+    //6. Operation procedure
+    struct sembuf sem_b;
+    sem_b.sem_num = 0;      //first sem(index=0)
+    sem_b.sem_flg = SEM_UNDO;
+    sem_b.sem_op = 1;           //Increase 1,make sem=1
+    
+    while(1)
+    {
+        if(0 == (value = semctl(sem_id, 0, GETVAL)))
+        {
+            printf("\nNow, snd message process running:\n");
+            printf("\tInput the snd message:  ");
+            scanf("%s", shm_ptr);
+
+            if(-1 == semop(sem_id, &sem_b, 1))
+            {
+                perror("semop");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        //if enter "end", then end the process
+        if(0 == (strcmp(shm_ptr ,"end")))
+        {
+            printf("\nExit sender process now!\n");
+            break;
+        }
+    }
+
+    shmdt(shm_ptr);
+
+    return 0;
+}
